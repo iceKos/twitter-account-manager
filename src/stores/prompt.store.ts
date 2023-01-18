a95f349f8ba6857ea3751c50f9bc6f29a01c44ec
ice.kos <ice@kos.co.th>
2023-01-18 07:32:19 +0000

    update prompt

--- src/stores/prompt.store.ts
+++ src/stores/prompt.store.ts
@@ -7,7 +7,7 @@ export const usePromptStore = defineStore("prompt", () => {
         initPrompt = [
             "can you create short positive good sentence for Nakamoto.games platform play to earn and add #NAKA , emoji",
             "can you make shot positive good word say to some platform and add emoji",
-            "Please provide me with short good sentence and add #NAKA in your sentence"
+            "Please provide me with short good sentence say to some platform and add #NAKA in your sentence"
         ]
     }
     var prompts: any = ref(initPrompt)
